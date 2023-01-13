c2896b59a42bf2f1fac1eeb28e215dd2b787fbbe
kkaoww <120167981+kkaoww@users.noreply.github.com>
2023-01-13 17:05:29 +0700

    Update lab14_1.cpp

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,30 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double M[],int N, double T[]){
+    for(int i=0; i < N;i++){
+        T[0] += M[i];
+    }
+    T[0] = T[0]/N;
+    double mean = T[0] ;
+    for(int j = 0; j < N ; j++){
+        T[1] += pow((M[j]-mean),2);
+    }
+    T[1] = sqrt(T[1]/(N));
+    for(int k=0;k<N;k++){
+        T[2] +=log10 (M[k]);
+    }
+    T[2] = T[2]/N;
+    T[2] = pow(10,T[2]);
+    for(int m=0;m<N;m++){
+        T[3] += (1/M[m]) ;
+    }
+    T[3] = N/T[3];
+    T[4]=M[0];
+    T[5]=M[0];
+    for(int n=0;n<N;n++){
+        if(M[n]>T[4])T[4]=M[n];
+        if(M[n]<T[5])T[5]=M[n];
+    }
+}
